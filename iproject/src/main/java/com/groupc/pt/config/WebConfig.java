47f052632b031110e17deca235eba4105abb9712
Zvirblyte <48699149+Zvirblyte@users.noreply.github.com>
2019-09-05 12:03:49 -0400

    Update WebConfig.java
    Bean not needed here already in xml web config

--- iproject/src/main/java/com/groupc/pt/config/WebConfig.java
+++ iproject/src/main/java/com/groupc/pt/config/WebConfig.java
@@ -9,12 +9,5 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @EnableWebMvc
 @ComponentScan(basePackages = { "com.groupc.pt.controller" })
 public class WebConfig extends WebMvcConfigurerAdapter {
-	
-	@Bean
-	public InternalResourceViewResolver resolver() {
-		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-		resolver.setPrefix("/WEB-INF/views/");
-		resolver.setSuffix(".jsp");
-		return resolver;
-	}
+
 }
